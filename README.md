b064db0bfeb264bdd1727f816cbda3c3cd80e1c3
Zxilly <zxilly@outlook.com>
2024-09-08 00:11:25 +0800

    feat: add internlm

--- README.md
+++ README.md
@@ -1,8 +1,22 @@
 # 仓颉 Lora
 
-基于 Qwen2-7B-Instruct 的仓颉语言 Lora，基于标准库文档和 Cangjie-TPC 代码训练。
+仓颉语言 Lora，基于标准库文档和 Cangjie-TPC 代码训练。
 
-[Lora](https://huggingface.co/Zxilly/qwen2-cangjie-lora)
+> 建议使用 InternLM2.5 版本
+
+## 基于 InternLM2.5-7B-Chat
+
+[Lora](https://github.com/Zxilly/Cangjie-Lora/releases/tag/Lora-3)
+
+[完整模型](https://huggingface.co/Zxilly/intern2_5-cangjie)
+
+[Q5_K_M GGUF量化](https://huggingface.co/Zxilly/intern2_5-cangjie-Q5_K_M-GGUF)
+
+[Ollama 模型](https://ollama.com/Zxilly/intern2_5-cangjie)
+
+## 基于 Qwen2-7B-Instruct
+
+[Lora](https://github.com/Zxilly/Cangjie-Lora/releases/tag/Lora-2)
 
 [完整模型](https://huggingface.co/Zxilly/qwen2-cangjie)
 
@@ -15,7 +29,7 @@
 ### Ollama
 
 ```bash
-ollama run Zxilly/qwen2-cangjie
+ollama run Zxilly/intern2_5-cangjie
 ```
 
 ### llama.cpp
@@ -23,11 +37,11 @@ ollama run Zxilly/qwen2-cangjie
 #### Cli
 
 ```bash
-llama-cli --hf-repo Zxilly/qwen2-cangjie-Q5_K_M-GGUF --hf-file qwen2-cangjie-q5_k_m.gguf -p "用仓颉写一个 Hello World"
+llama-cli --hf-repo Zxilly/intern2_5-cangjie-Q5_K_M-GGUF --hf-file intern2_5-cangjie-q5_k_m.gguf -p "用仓颉写一个 Hello World"
 ```
 
 #### Server
 
 ```bash
-llama-server --hf-repo Zxilly/qwen2-cangjie-Q5_K_M-GGUF --hf-file qwen2-cangjie-q5_k_m.gguf -c 2048
+llama-server --hf-repo Zxilly/intern2_5-cangjie-Q5_K_M-GGUF --hf-file intern2_5-cangjie-q5_k_m.gguf -c 2048
 ```
--- fix.py
+++ fix.py
@@ -1,38 +1,10 @@
 import json
 import os
 import re
-import magic
-from markdownify import markdownify as md
-
-
-def convert_html_to_markdown(html_content):
-    return md(html_content)
-
-
-def get_all_html_files(directory):
-    html_files = []
-    for root, dirs, files in os.walk(directory):
-        for file in files:
-            if file.endswith('.html'):
-                html_files.append(os.path.join(root, file))
-    return sorted(html_files)
-
-
-def split_content(content, max_length=100000):
-    parts = []
-    while len(content) > max_length:
-        split_index = content.rfind('\n', 0, max_length)
-        if split_index == -1:
-            split_index = max_length
-        parts.append(content[:split_index])
-        content = content[split_index:].lstrip()
-    parts.append(content)
-    return parts
 
 
 def get_all_code_files(directory) -> list[str]:
     code_files = []
-    m = magic.Magic(mime=True)
 
     for root, dirs, files in os.walk(directory):
         # 移除 .git 文件夹
@@ -44,86 +16,43 @@ def get_all_code_files(directory) -> list[str]:
                 i += 1
 
         for file in files:
-            name = os.path.join(root, file)
-            r = m.from_file(name)
-            if r.startswith('text/'):
-                if name.endswith('.json'):
-                    continue
-                if name.endswith('.yaml'):
-                    continue
-                if name.find('cjcov') != -1:
-                    continue
-                if name.endswith('.html'):
-                    continue
-                if name.endswith('.lock'):
-                    continue
-
+            name = str(os.path.join(root, file))
+            if file.endswith('.cj'):
                 code_files.append(name)
-                print(name, r)
+
     return code_files
 
 
 m = re.compile(r'\n{3,}')
 
 # 去除无意义的连续换行
-def clean(s: str) -> str:
-    return re.sub(m, '\n\n', s)
-
 def main():
-    docs_dir = 'docs'
-    output_file = 'cangjie.json'
-    
-    html_files = get_all_html_files(docs_dir)
+    output_file = 'cangjie-code.json'
 
     data = []
 
-    # code_files = get_all_code_files('code')
-    # for code_file in code_files:
-    #     print(f"Processing {code_file}...")
+    code_files = get_all_code_files('code')
+    for code_file in code_files:
+        print(f"Processing {code_file}...")
         
-    #     try:
-    #         with open(code_file, 'r', encoding='utf-8') as infile:
-    #             code_content = infile.read()
-    #     except UnicodeDecodeError:
-    #         print(f"Error reading {code_file}, skipping...")
-    #         continue
+        try:
+            with open(code_file, 'r', encoding='utf-8') as infile:
+                code_content = infile.read()
+        except UnicodeDecodeError:
+            print(f"Error reading {code_file}, skipping...")
+            continue
         
-    #     relative_path = os.path.relpath(code_file, 'code')
-    #     markdown_content = f"# {relative_path}\n\n```\n{code_content}\n```"
-        
-    #     content_parts = split_content(markdown_content)
-        
-    #     for part in content_parts:
-    #         part_data = {
-    #             "text": part,
-    #         }
-    #         data.append(part_data)
-    
-    for html_file in html_files:
-        print(f"Processing {html_file}...")
-        
-        with open(html_file, 'r', encoding='utf-8') as infile:
-            html_content = infile.read()
+        relative_path = os.path.relpath(code_file, 'code')
+        markdown_content = f"# 文件路径：{relative_path}\n\n```cangjie\n{code_content}\n```"
+        data.append({
+            "input": f"请给我提供{relative_path}的内容。",
+            "output": markdown_content
+        })
 
-        # 移除 <main></main> 以外的内容
-        html_content = re.search(r'<main>(.*)</main>', html_content, re.S).group(1)
-        
-        markdown_content = convert_html_to_markdown(html_content)
-        markdown_content = clean(markdown_content)
-        
-        relative_path = os.path.relpath(html_file, docs_dir)\
-        
-        relative_path = relative_path.replace('.html', '')
-        relative_path = relative_path.replace('\\', '-')
-        relative_path = relative_path.replace('/', '-')
-        
-        with open(f'./md/{relative_path}.md', 'w', encoding='utf-8') as outfile:
-            outfile.write(markdown_content)
-
-    # with open(output_file, 'w', encoding='utf-8') as outfile:
-    #     json.dump(data, outfile, ensure_ascii=False, indent=2)
+    with open(output_file, 'w', encoding='utf-8') as outfile:
+        json.dump(data, outfile, ensure_ascii=False, indent=2)
     
-    print(f"All HTML files have been converted and combined into {output_file}")
+    print(f"Processed {len(code_files)} code files. Output saved to {output_file}")
 
 if __name__ == "__main__":
     main()
